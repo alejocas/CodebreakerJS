a6c4de7cadf348e6ea52d9c254ed7fb996c82a49
Alejo Castaño <alejocas98@gmail.com>
2018-11-10 12:16:03 -0500

    POST ready, test pending

--- services/postCodebreaker.js
+++ services/postCodebreaker.js
@@ -1,5 +1,13 @@
 const { codebreakerObject } = require('../config');
 
 const changeCode = (req, res) => {
-    
+    const newSecret = req.body.secret;
+    codebreakerObject.setSecret(String(newSecret));
+    res.send({
+        answer: "Secret changed"
+    });
+}
+
+module.exports = {
+    changeCode
 }
