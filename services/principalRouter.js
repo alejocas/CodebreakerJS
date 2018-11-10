c6c2203c46a3973deb7b37396f7f756aeb338b53
Alejo Castaño <alejocas98@gmail.com>
2018-11-10 12:19:04 -0500

    POST ready

--- services/principalRouter.js
+++ services/principalRouter.js
@@ -1,9 +1,11 @@
 const { express } = require('../config');
 const { guessCode } = require('./getCodebreaker');
+const { changeCode } = require('./postCodebreaker');
 
 const router = express.Router()
 
 router.get('/guess/:number', guessCode);
+router.post('/guess', changeCode);
 
 module.exports = {
     router
